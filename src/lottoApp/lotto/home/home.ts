b0c4a390b8f419a1004b170eaeaf475694ac0c56
byverdu <byverdu@gmail.com>
2016-09-12 22:43:50 +0100

    added logic for lottoPage

--- src/lottoApp/lotto/home/home.ts
+++ src/lottoApp/lotto/home/home.ts
@@ -8,18 +8,39 @@ import LottoRouter from '../lotto';
 @autoinject
 export default class Home {
   public raffleType: LottoModel;
-  public combiToSave: Array<number>;
+  public combiToSave: Array<number> = [];
   public subscriber: Disposable;
   constructor (
     private fetchApi: FetchApi,
     private lottoUtils: LottoUtils,
     private windowStorage: WindowStorage,
     private lottoRouter: LottoRouter,
-    private bindingEngine: BindingEngine
+    private bindingEngine: BindingEngine,
+    private element: Element
   ) {
       this.subscriber = this.bindingEngine
         .propertyObserver(this.lottoRouter, 'raffleType')
         .subscribe(this.lottoRouterData.bind(this));
+  }
+
+  public addBallToCombiToSave(ball) {
+    const toNumber = Number(ball);
+    const numberConverted: number = toNumber + 1;
+    const indexChecked = this.combiToSave.indexOf(numberConverted);
+
+    if (this.combiToSave.indexOf(numberConverted) === -1) {
+      this.combiToSave.push(numberConverted);
+      this.combiToSave.sort(( a, b ) => a - b );
+    } else {
+      this.combiToSave.splice(indexChecked, 1);
+    }
+  }
+
+  public clearAndUncheck() {
+    console.log(this.element, 'balllllllllll')
+    const x = this.element.querySelectorAll('.test:checked');
+    [].forEach.call(x, item => item.checked = false);
+    this.combiToSave = [];
   }
 
   deactivate(){
@@ -32,10 +53,12 @@ export default class Home {
       this.windowStorage.setWindowStorage(this.raffleType.lottoID, randToString);
       console.log(this.raffleType, randToString, 'saveSelectedNumbers');
       this.setCombinations();
+      this.clearAndUncheck();
     }
   }
 
   public getRandomBallsByLotto() {
+    this.clearAndUncheck();
     this.combiToSave = this.lottoUtils.getRandomBallsByLotto(this.raffleType);
     console.log(this.combiToSave, 'getRandomBallsByLotto');
   }
