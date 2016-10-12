0dc9f051888af969b1e2a9ad624203191431f698
byverdu <byverdu@gmail.com>
2016-10-12 23:39:24 +0100

    wip in winnersPage

--- src/lottoApp/lotto/winners/winners.ts
+++ src/lottoApp/lotto/winners/winners.ts
@@ -1,3 +1,22 @@
-export default class Winners {
+import {autoinject} from 'aurelia-framework'
+import FetchApi from '../../../services/fetchApi';
 
+@autoinject
+export default class Winners {
+  public winnersData: any;
+  public tableData: Array<string>;
+  constructor(
+    private fetchApi: FetchApi
+  ){
+  }
+
+  activate(params) {
+    this.fetchApi.getLottoDocById(`${params.lottoID}Winner`)
+      .then(
+        response => {
+          this.winnersData = response[`${params.lottoID}Winner`];
+          this.tableData = Object.keys(this.winnersData.allWinners[0]);
+        }
+      );
+  }
 }
